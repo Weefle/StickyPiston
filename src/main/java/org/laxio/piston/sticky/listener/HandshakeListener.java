fefe8cdb7b875ad805a31a9a4e2bdc7d634c2171
Harry Fox <harry.fox.ic@icloud.com>
2017-12-31 14:05:54 +0000

    Only Play state needs to be translated for now

--- src/main/java/org/laxio/piston/sticky/listener/HandshakeListener.java
+++ src/main/java/org/laxio/piston/sticky/listener/HandshakeListener.java
@@ -1,10 +1,13 @@
 package org.laxio.piston.sticky.listener;
 
+import org.laxio.piston.piston.chat.ChatColor;
+import org.laxio.piston.piston.chat.MessageBuilder;
 import org.laxio.piston.piston.event.PacketHandler;
 import org.laxio.piston.piston.event.listener.Listener;
 import org.laxio.piston.piston.protocol.ProtocolState;
 import org.laxio.piston.protocol.v340.netty.NetworkClient;
 import org.laxio.piston.protocol.v340.packet.handshake.server.HandshakePacket;
+import org.laxio.piston.protocol.v340.packet.login.client.DisconnectPacket;
 
 public class HandshakeListener implements Listener {
 
@@ -13,7 +16,14 @@ public class HandshakeListener implements Listener {
         NetworkClient client = (NetworkClient) packet.getConnection();
         if (packet.getNextState() == ProtocolState.LOGIN) {
             if (client.getServer().getProtocol().getVersion() != packet.getProtocolVersion()) {
+                MessageBuilder builder = MessageBuilder.builder();
+                if (client.getServer().getProtocol().getVersion() > packet.getProtocolVersion()) {
+                    builder.message(ChatColor.RED + "Client out of date");
+                } else {
+                    builder.message(ChatColor.RED + "Server out of date");
+                }
 
+                packet.reply(new DisconnectPacket(builder.build()));
             }
         }
     }
